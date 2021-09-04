5fc1825507ade981241eb4ac77e22c1b9bafc2fe
Sina Marefat <Sinamatefat@gmail.com>
2021-09-04 11:52:47 +0430

    initialized and made first classes

---
+++ src/main/java/ir/tutorial/hello/helloController.java
@@ -0,0 +1,13 @@
+package ir.tutorial.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+
+    @RequestMapping("/hello")
+    public String sayHi(){
+        return "HI";
+    }
+}
